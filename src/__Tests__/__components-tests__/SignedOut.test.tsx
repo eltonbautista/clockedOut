d4456123963fd76c1ab8fc756840ea39286c7a69
Elton Bautista <eltonbestaha@gmail.com>
2022-06-07 09:39:50 -0600

    Fixed tests for SignedOut, looks like I didn't have my assertions in the asme scope as my render

--- src/__Tests__/__components-tests__/SignedOut.test.tsx
+++ src/__Tests__/__components-tests__/SignedOut.test.tsx
@@ -1,5 +1,8 @@
 import { getByText, render, screen } from "@testing-library/react";
 import React from "react";
+import { act } from "react-dom/test-utils";
+import userEvent from "@testing-library/user-event";
+
 import SignedOut from "../../Views/SignedOut";
 
 import { SODescriptionContainers, SOButtons } from "../../Components/SignedOut/SOComponents";
@@ -10,22 +13,28 @@ describe('Tests for my SignedOut component', () => {
     const text = screen.getByText(/hello world/i);
 
     expect(text).toBeInTheDocument();
-
-  })
-
-});
-
-describe('Tests for SODescriptionContainers component', () => {
+  });
 
   it('SODescriptionContainers should render a div and img with correct properties', () => {
     render(<SODescriptionContainers hText="header" imgAlt="img alt" imgSrc="img src" pText="Paragraph" />)
-  })
 
-  const descriptionH = screen.getByRole('header');
-  const imgSrc = screen.getByRole('img');
+    const descriptionH = screen.getByText('header');
+    const imgSrc = screen.getByRole('img');
 
-  expect(descriptionH).toBeInTheDocument();
-  expect(imgSrc).toStringContaining('img src')
+    expect(descriptionH).toBeInTheDocument();
+    expect(imgSrc).toHaveAttribute('src', 'img src');
+  });
+
+  it('should render SOButtons with dynamic props', () => {
+    render(<SOButtons bgColor="black" color="white" size="12px">login</SOButtons>)
+
+    const SOButton = screen.getByText(/login/i);
+    expect(SOButton).toBeInTheDocument();
+    expect(SOButton).toHaveStyle('color: white');
+    expect(SOButton).toHaveStyle('background-color: black');
+    expect(SOButton).toHaveStyle('font-size: 12px');
+
+  });
 
 });
 
